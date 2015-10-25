abf48880503880eb02ded45176fb2e5ba4808ccf
Dean Radcliffe <deanmisc@yahoo.com>
2015-10-24 23:06:27 -0400

    Moar migrating 2 react !
     @elijahlofgren !!

--- client/components/chessLayout.jsx
+++ client/components/chessLayout.jsx
@@ -1,1 +1,22 @@
-ChessLayout = BlazeToReact("layout")
+ChessLayout = React.createClass({
+  componentDidMount(){
+    // Reactive dependency upon MovesCollection.find()
+    // - any time the set of moves changes, render the
+    //   board of the last one
+    Tracker.autorun(function () {
+      var moves = MovesCollection.find().fetch();
+      if (moves.length > 0) {
+        Board.render(_.last(moves).board);
+      }
+    });
+
+  },
+  render(){
+    return(
+      <div>
+        <h1>Yo React, baby!</h1>
+        <div id="chessboard-js-board"/>
+      </div>
+    )
+  }
+})
--- client/views/board.js
+++ client/views/board.js
@@ -1,12 +1,3 @@
-// Reactive dependency upon MovesCollection.find()
-// - any time the set of moves changes, render the
-//   board of the last one
-Tracker.autorun(function () {
-  var moves = MovesCollection.find().fetch();
-  if (moves.length > 0) {
-    Board.render(_.last(moves).board);
-  }
-});
 
 Template.board.helpers({
   moves: function () {
