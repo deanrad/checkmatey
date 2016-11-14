8e15fd4761dd44af6e7dc3b14686f1a7160cc5f3
Dean Radcliffe <deanmisc@yahoo.com>
2016-11-14 15:54:44 -0600

    Add a Collection, pubsub and seeding, and you have a fullstack reactive app. Boom.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,17 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor'
+import { Moves } from '/lib/collections'
 
 Meteor.startup(() => {
-  // code to run on server at startup
-});
+  if (Moves.find().count() === 0) {
+    Moves.insert({
+        from: null,
+        to: null,
+        position: {
+            a8:'bR', b8:'bN', c8:'bB', d8:'bQ', e8:'bK', f8:'bB', g8:'bN', h8:'bR',
+            a7:'bP', b7:'bP', c7:'bP', d7:'bP', e7:'bP', f7:'bP', g7:'bP', h7:'bP',
+            a2:'wP', b2:'wP', c2:'wP', d2:'wP', e2:'wP', f2:'wP', g2:'wP', h2:'wP',
+            a1:'wR', b1:'wN', c1:'wB', d1:'wQ', e1:'wK', f1:'wB', g1:'wN', h1:'wR'
+        }
+    })
+  }
+})
