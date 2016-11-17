e5e1bad68e2219c1146aedf58db5d2842b2275c0
Dean Radcliffe <deanmisc@yahoo.com>
2016-11-17 12:06:25 -0600

    WIP

--- packages/dispatch/index.js
+++ packages/dispatch/index.js
@@ -16,33 +16,8 @@ const action$ = actions.asObservable()
 //
 // It will likely export `dispatch` to containers, that will share it via props.
 export const DispatchWith = ({ Actions, PayloadSchema, Consequences, Reducers, Collections }) => {
-
     // UniMethod: returns Promise from the client, runs sync on server
-    let universalMethod = getDispatch({ Actions, PayloadSchema, Consequences, Reducers, Collections })
-
-    let consequence$ = action$
-        .map(action => {
-            let epic = consequencesOf(action, action$, Consequences).startWith(action)
-            // epic.subscribe({complete: () => {console.log('DAC>*')}})
-            return epic
-        })
-        .mergeAll()
-
-    consequence$.subscribe(consequence => {console.log('DAC>', consequence.toJS()) })
-
-    consequence$
-        .filter(a => ! a.getIn(['meta', 'mayBeFulfilledLocally']))
-        .map(a => a.toJS())
-        .subscribe(universalMethod)
-
     return {
-        // dispatch puts an immutable action onto the stream, which subscribers will react to accordingly
-        dispatch: (action) => {
-            console.log('D>', action)
-            let iAction = stampFields(fromJS(action))
-            actions.next(iAction)
-        },
-        // consequence$ is the merged stream of all items, prior to being considered for sending
-        consequence$
+        dispatch: getDispatch({ Actions, PayloadSchema, Consequences, Reducers, Collections })
     }
 }
--- packages/dispatch/package.js
+++ packages/dispatch/package.js
@@ -1,14 +1,16 @@
 Package.describe({
   name: 'deanius:dispatch',
-  version: '0.7.0',
+  version: '0.8.9',
   summary: 'Business Process Layer',
 });
 
 Npm.depends({
+  'lodash': '4.17.1',
   'mongodb-diff': '0.4.3',
   redux: '3.6.0',
   'redux-act': '1.1.0',
   rxjs: '5.0.0-rc.3',
+  'redux-observable': '0.12.1',
   uuid: '2.0.3',
 });
 
